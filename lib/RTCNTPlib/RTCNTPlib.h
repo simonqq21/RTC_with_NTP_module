9c94f685116384af3b155a73d5861e0a0d12cbcb
Simon <Simon@rooftopbim.com>
2024-07-17 21:12:58 +0800

    x

---
+++ lib/RTCNTPlib/RTCNTPlib.h
@@ -0,0 +1,16 @@
+#ifndef RTCNTPLIB_H
+#define RTCNTPLIB_H
+#include <Arduino.h>
+#include <RTClib.h>
+#include <TimeLib.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+class RTCNTP {
+    public:
+        RTCNTP();
+    private: 
+    
+};
+
+#endif
